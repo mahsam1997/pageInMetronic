e30af1c882c899cb7cc4eb7505fb15d5b9575239
Mahdi zoraghi sheibak <mahdi.zoraghi.sheibak@gmail.com>
2021-07-11 13:34:06 -0700

    Feat: Create forget pass page

--- src/app/modules/Auth/pages/ForgotPassword.js
+++ src/app/modules/Auth/pages/ForgotPassword.js
@@ -3,121 +3,150 @@ import { useFormik } from "formik";
 import { connect } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 import * as Yup from "yup";
-import { injectIntl } from "react-intl";
+import { FormattedMessage, injectIntl } from "react-intl";
 import * as auth from "../_redux/authRedux";
 import { requestPassword } from "../_redux/authCrud";
+import routes from "../../../router/routes.json";
 
 const initialValues = {
-  email: "",
+   email: "",
 };
 
 function ForgotPassword(props) {
-  const { intl } = props;
-  const [isRequested, setIsRequested] = useState(false);
-  const ForgotPasswordSchema = Yup.object().shape({
-    email: Yup.string()
-      .email("Wrong email format")
-      .min(3, "Minimum 3 symbols")
-      .max(50, "Maximum 50 symbols")
-      .required(
-        intl.formatMessage({
-          id: "AUTH.VALIDATION.REQUIRED_FIELD",
-        })
-      ),
-  });
+   const { intl } = props;
+   const [isRequested, setIsRequested] = useState(false);
+   const ForgotPasswordSchema = Yup.object().shape({
+      email: Yup.string()
+         .email(
+            intl.formatMessage({
+               id: "AUTH.VALIDATION.EMAIL",
+            })
+         )
+         .min(
+            3,
+            intl.formatMessage({
+               id: "AUTH.VALIDATION.MIN_LENGTH_FIELD",
+            })
+         )
+         .max(
+            50,
+            intl.formatMessage({
+               id: "AUTH.VALIDATION.MAX_LENGTH_FIELD",
+            })
+         )
+         .required(
+            intl.formatMessage({
+               id: "AUTH.VALIDATION.REQUIRED_FIELD",
+            })
+         ),
+   });
 
-  const getInputClasses = (fieldname) => {
-    if (formik.touched[fieldname] && formik.errors[fieldname]) {
-      return "is-invalid";
-    }
+   const getInputClasses = fieldname => {
+      if (formik.touched[fieldname] && formik.errors[fieldname]) {
+         return "is-invalid";
+      }
 
-    if (formik.touched[fieldname] && !formik.errors[fieldname]) {
-      return "is-valid";
-    }
+      if (formik.touched[fieldname] && !formik.errors[fieldname]) {
+         return "is-valid";
+      }
 
-    return "";
-  };
+      return "";
+   };
 
-  const formik = useFormik({
-    initialValues,
-    validationSchema: ForgotPasswordSchema,
-    onSubmit: (values, { setStatus, setSubmitting }) => {
-      requestPassword(values.email)
-        .then(() => setIsRequested(true))
-        .catch(() => {
-          setIsRequested(false);
-          setSubmitting(false);
-          setStatus(
-            intl.formatMessage(
-              { id: "AUTH.VALIDATION.NOT_FOUND" },
-              { name: values.email }
-            )
-          );
-        });
-    },
-  });
+   const formik = useFormik({
+      initialValues,
+      validationSchema: ForgotPasswordSchema,
+      onSubmit: (values, { setStatus, setSubmitting }) => {
+         requestPassword(values.email)
+            .then(() => setIsRequested(true))
+            .catch(() => {
+               setIsRequested(false);
+               setSubmitting(false);
+               setStatus(
+                  intl.formatMessage(
+                     { id: "AUTH.VALIDATION.NOT_FOUND" },
+                     { name: values.email }
+                  )
+               );
+            });
+      },
+   });
 
-  return (
-    <>
-      {isRequested && <Redirect to="/auth" />}
-      {!isRequested && (
-        <div className="login-form login-forgot" style={{ display: "block" }}>
-          <div className="text-center mb-10 mb-lg-20">
-            <h3 className="font-size-h1">Forgotten Password ?</h3>
-            <div className="text-muted font-weight-bold">
-              Enter your email to reset your password
+   return (
+      <>
+         {isRequested && <Redirect to={routes.AUTH} />}
+         {!isRequested && (
+            <div
+               className="login-form login-forgot"
+               style={{ display: "block" }}
+            >
+               <div className="mb-5 mb-lg-10">
+                  <h3 className="font-size-h1">
+                     <FormattedMessage id="AUTH.FORGOT.TITLE" />
+                  </h3>
+                  <div className="text-muted font-weight-bold">
+                     <FormattedMessage id="AUTH.FORGOT.DESC" />
+                  </div>
+               </div>
+               <form
+                  onSubmit={formik.handleSubmit}
+                  className="form fv-plugins-bootstrap fv-plugins-framework animated animate__animated animate__backInUp"
+               >
+                  {formik.status && (
+                     <div className="mb-10 alert alert-custom alert-light-danger alert-dismissible">
+                        <div className="alert-text font-weight-bold">
+                           {formik.status}
+                        </div>
+                     </div>
+                  )}
+                  <div className="form-group fv-plugins-icon-container">
+                     <label htmlFor="email">
+                        <FormattedMessage id="AUTH.INPUT.EMAIL" />
+                     </label>
+                     <input
+                        type="email"
+                        className={`form-control form-control-solid h-auto py-5 px-6 ${getInputClasses(
+                           "email"
+                        )}`}
+                        placeholder={intl.formatMessage({
+                           id: "AUTH.INPUT.EMAIL.PLACE",
+                        })}
+                        name="email"
+                        id="email"
+                        {...formik.getFieldProps("email")}
+                     />
+                     {formik.touched.email && formik.errors.email ? (
+                        <div className="fv-plugins-message-container">
+                           <div className="fv-help-block">
+                              {formik.errors.email}
+                           </div>
+                        </div>
+                     ) : null}
+                  </div>
+                  <div className="form-group d-flex ">
+                     <button
+                        id="kt_login_forgot_submit"
+                        type="submit"
+                        className="btn btn-primary font-weight-bold px-11 py-3 my-3"
+                        disabled={formik.isSubmitting}
+                     >
+                        <FormattedMessage id="AUTH.FORGOT.SEND" />
+                     </button>
+                     <Link to={routes.AUTH}>
+                        <button
+                           type="button"
+                           id="kt_login_forgot_cancel"
+                           className="btn btn-light-primary font-weight-bold px-13 py-3 my-3 mx-4"
+                        >
+                           <FormattedMessage id="AUTH.GENERAL.CANCEL" />
+                        </button>
+                     </Link>
+                  </div>
+               </form>
             </div>
-          </div>
-          <form
-            onSubmit={formik.handleSubmit}
-            className="form fv-plugins-bootstrap fv-plugins-framework animated animate__animated animate__backInUp"
-          >
-            {formik.status && (
-              <div className="mb-10 alert alert-custom alert-light-danger alert-dismissible">
-                <div className="alert-text font-weight-bold">
-                  {formik.status}
-                </div>
-              </div>
-            )}
-            <div className="form-group fv-plugins-icon-container">
-              <input
-                type="email"
-                className={`form-control form-control-solid h-auto py-5 px-6 ${getInputClasses(
-                  "email"
-                )}`}
-                name="email"
-                {...formik.getFieldProps("email")}
-              />
-              {formik.touched.email && formik.errors.email ? (
-                <div className="fv-plugins-message-container">
-                  <div className="fv-help-block">{formik.errors.email}</div>
-                </div>
-              ) : null}
-            </div>
-            <div className="form-group d-flex flex-wrap flex-center">
-              <button
-                id="kt_login_forgot_submit"
-                type="submit"
-                className="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-4"
-                disabled={formik.isSubmitting}
-              >
-                Submit
-              </button>
-              <Link to="/auth">
-                <button
-                  type="button"
-                  id="kt_login_forgot_cancel"
-                  className="btn btn-light-primary font-weight-bold px-9 py-4 my-3 mx-4"
-                >
-                  Cancel
-                </button>
-              </Link>
-            </div>
-          </form>
-        </div>
-      )}
-    </>
-  );
+         )}
+      </>
+   );
 }
 
 export default injectIntl(connect(null, auth.actions)(ForgotPassword));
