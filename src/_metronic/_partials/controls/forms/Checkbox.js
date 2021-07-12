c982c14a38a0d5465f9d93e03e2752d6f07a7dd6
Mahdi zoraghi sheibak <mahdi.zoraghi.sheibak@gmail.com>
2021-07-12 06:58:30 -0700

    Refactor: refactor register page

--- src/_metronic/_partials/controls/forms/Checkbox.js
+++ src/_metronic/_partials/controls/forms/Checkbox.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 export function Checkbox({ isSelected, onChange, children, id }) {
-  return (
-    <>
-      <input type="checkbox" style={{ display: "none" }} id={id} />
-      <label className="checkbox checkbox-lg checkbox-single">
-        <input type="checkbox" checked={isSelected} onChange={onChange} />
-        {children}
-        <span />
-      </label>
-    </>
-  );
+   return (
+      <>
+         <input type="checkbox" style={{ display: "none" }} id={id} />
+         <label className="checkbox checkbox-lg checkbox-single ltr">
+            <input type="checkbox" checked={isSelected} onChange={onChange} />
+            <main>{children}</main>
+            <span />
+         </label>
+      </>
+   );
 }
---
+++ src/app/components/TextError.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TextError = errorMessage => (
+   <div className="fv-plugins-message-container">
+      <div className="fv-help-block">{errorMessage}</div>
+   </div>
+);
+
+export default TextError;
