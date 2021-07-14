6dd5eecc3c0e67b228335863c1350a7059e215c9
Mahdi zoraghi sheibak <mahdi.zoraghi.sheibak@gmail.com>
2021-07-14 12:11:47 -0700

    Fix: Fix input validation UI

--- src/_metronic/_partials/controls/forms/Checkbox.js
+++ src/_metronic/_partials/controls/forms/Checkbox.js
@@ -6,8 +6,8 @@ export function Checkbox({ isSelected, onChange, children, id }) {
          <input type="checkbox" style={{ display: "none" }} id={id} />
          <label className="checkbox checkbox-lg checkbox-single ltr">
             <input type="checkbox" checked={isSelected} onChange={onChange} />
-            <main>{children}</main>
             <span />
+            <main>{children}</main>
          </label>
       </>
    );
