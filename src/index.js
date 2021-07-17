2137f442d3c6665d5ac2cffd8f0ce6201b4dac69
Mahdi zoraghi sheibak <mahdi.zoraghi.sheibak@gmail.com>
2021-07-17 03:30:02 -0700

    Fix: fix conditional css import base on direction

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
-// import "./index.scss"; // Standard version
-// import "./sass/style.react.rtl.css"; // RTL version
+
 import "./_metronic/_assets/plugins/keenthemes-icons/font/ki.css";
 import "socicon/css/socicon.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -22,6 +21,7 @@ import {
    MetronicSubheaderProvider,
 } from "./_metronic/layout";
 import { MetronicI18nProvider } from "./_metronic/i18n";
+
 import "./app/Assets/fonts/Vazir/fontVazir.css";
 
 /**
@@ -44,16 +44,6 @@ const { PUBLIC_URL } = process.env;
  *
  * @see https://github.com/axios/axios#interceptors
  */
-
-const language =
-  JSON.parse(localStorage.getItem("i18nConfig"))?.selectedLang || "en";
-const body = document.getElementById("kt_body");
-
-const isEnglish = language === "en";
-isEnglish ? require("./index.scss") : require("./sass/style.react.rtl.css");
-body.direction = isEnglish ? "ltr" : "rtl";
-body.dir = isEnglish ? "ltr" : "rtl";
-body.style.direction = isEnglish ? "ltr" : "rtl";
 
 ReactDOM.render(
    <MetronicI18nProvider>
