54f9a217f5daf6b61e7db037e4dbb5a85af90f2a
Kian Jalilian <kian2000.jalilian@gmail.com>
2021-07-22 02:13:55 +0430

    Feat: fix direction problem

--- src/app/components/AppDirection.jsx
+++ src/app/components/AppDirection.jsx
@@ -1,29 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import i18next from "i18next";
+import { useTranslation } from "react-i18next";
 
 const RtlStyles = React.lazy(() => import("./RtlStyles"));
 const LtrStyles = React.lazy(() => import("./LtrStyles"));
 
-const language =
-   JSON.parse(localStorage.getItem("i18nConfig"))?.selectedLang || "en";
-const body = document.getElementById("kt_body");
-
-const isEnglish = language === "en";
-body.direction = isEnglish ? "ltr" : "rtl";
-body.dir = isEnglish ? "ltr" : "rtl";
-body.style.direction = isEnglish ? "ltr" : "rtl";
-
-// const appDirection = i18next.dir();
-// body.direction = appDirection;
-// body.dir = appDirection;
-// body.style.direction = appDirection;
-
 const AppDirection = () => {
-   console.log("App direction: ", i18next.dir());
+   const [appDirection, setAppDirection] = useState();
+   const { i18n } = useTranslation();
 
-   return isEnglish ? <LtrStyles /> : <RtlStyles />;
+   useEffect(() => {
+      console.log("in useEffect: ", i18n);
+      if (i18n.isInitialized) {
+         const direction = i18next.dir();
 
-   //    return appDirection === "ltr" ? <LtrStyles /> : <RtlStyles />;
+         document.body.direction = direction;
+         document.body.dir = direction;
+         document.body.style.direction = direction;
+         setAppDirection(direction);
+      }
+   }, [i18n]);
+
+   return appDirection === "ltr" ? <LtrStyles /> : <RtlStyles />;
 };
 
 export default AppDirection;
