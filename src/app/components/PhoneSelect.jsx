27327bcad345fcae8a7da389f299c6a2316d04fa
Mahdi zoraghi sheibak <mahdi.zoraghi.sheibak@gmail.com>
2021-07-22 12:50:55 -0700

    Fix: customize PhoneSelect.jsx

--- src/app/components/PhoneSelect.jsx
+++ src/app/components/PhoneSelect.jsx
@@ -1,44 +1,5 @@
 import React from "react";
-
-<<<<<<< HEAD
-const PhoneSelect = ({ options, value, ...props }) => {
-   const selectStyles = {
-      control: styles => ({
-         ...styles,
-         backgroundColor: "#ffffff",
-         width: "90px",
-         padding: "7px 0",
-         borderRadius: "12px",
-         marginRight: "10px",
-         marginLeft: "10px",
-         border: "none",
-         direction: "ltr",
-         paddingLeft: 5,
-         //  color: "#A7A8BB",
-         fontStyle: "normal",
-         fontWeight: 500,
-         fontSize: "14px",
-         lineHeight: "24px",
-      }),
-      option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-         return {
-            ...styles,
-            fontStyle: "normal",
-            fontWeight: 500,
-            fontSize: "14px",
-            lineHeight: "24px",
-            // color: "#A7A8BB",
-         };
-      },
-      // input: styles => ({ ...styles, ...dot() }),
-      // placeholder: styles => ({ ...styles, ...dot() }),
-      // singleValue: (styles, { data }) => ({ ...styles, ...dot(data.color) }),
-   };
-
-   const defaultValue = options.filter(option => option.value === value)[0];
-=======
 import CustomSelect from "./common/CustomSelect";
->>>>>>> user-management-api
 
 const PhoneSelect = ({ options, value }) => {
    return (
